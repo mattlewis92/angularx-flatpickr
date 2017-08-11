58382b6e4aef3a222241dcf9ffbfed607fc7c181
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-08-11 17:06:24 +0100

    test: use mocha reporter

--- karma.conf.ts
+++ karma.conf.ts
@@ -81,10 +81,15 @@ export default (config: any) => {
       'text/x-typescript': ['ts']
     },
 
+    mochaReporter: {
+      showDiff: true,
+      output: 'autowatch'
+    },
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage-istanbul'],
+    reporters: ['mocha', 'coverage-istanbul'],
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
